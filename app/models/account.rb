3c6b2ff0bff6c9daef7fcddb6b441bbc1f40c9c9
Rajeswari <krajeswarimca@gmail.com>
2012-03-25 11:40:34 +0530

    added attr_protected

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,4 @@
 class Account < ActiveRecord::Base
   attr_accessible :user, :acc_type, :area
+  attr_protected :user, :acc_type, :area
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ActiveRecord::Base
   attr_accessible :title, :price, :description
+  attr_protected :title, :price, :description
 end
